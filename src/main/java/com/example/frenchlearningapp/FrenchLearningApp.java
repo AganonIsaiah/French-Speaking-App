95cb708563548a14f7a0be5d1cce4d64eab825f2
Isaiah Aganon <isaiahaganon0@gmail.com>
2024-07-02 23:39:32 -0400

    First Commit

---
+++ src/main/java/com/example/frenchlearningapp/FrenchLearningApp.java
@@ -0,0 +1,13 @@
+package com.example.frenchlearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchLearningApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrenchLearningApp.class, args);
+    }
+
+}
