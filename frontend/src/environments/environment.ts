d508bb13f834b7bb1b53c8bc577bd68902f618a8
AganonIsaiah <isaiahaganon0@gmail.com>
2025-08-16 22:39:35 -0400

    Feat: config cors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ import { EnvironmentModel } from "../app/models/environment.model";
 
 export const environment: EnvironmentModel = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+   apiUrl: 'https://french-speaking-app.onrender.com'
 };
